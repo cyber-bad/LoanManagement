a86f6e8d516dfb997ae89a5c283e8d1724119736
prashanth <prashanth.s@gmail.com>
2019-10-13 14:20:04 +1100

    Added API calling the service.

---
+++ LoanManagement.API/Controllers/LoanController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LoanManagement.Model;
+using LoanManagement.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LoanManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoanController : ControllerBase
+    {
+
+        private readonly ILoanService loanService;
+        public LoanController(ILogger<LoanController> logger, ILoanService loansService)
+        {
+            loanService = loansService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Loan>>> GetLoans()
+        {
+            var loans = await loanService.GetLoans();
+
+            return Ok(loans);
+
+        }
+
+       
+    }
+}
---
+++ LoanManagement.API/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using LoanManagement.Data;
+using LoanManagement.Service;
+
+namespace LoanManagement.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //services.AddDbContext<LoanManagementDBContext>(options =>
+            //        options.UseSqlServer(Configuration.GetConnectionString("LoanManagementAPIContext")));
+
+            services.AddDbContext<LoanManagementDBContext>(options => 
+            options.UseInMemoryDatabase("LoanManagement"));
+
+            services.AddTransient<ILoanService, LoanService>();
+            services.AddTransient<ILoanRepository, LoanRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
